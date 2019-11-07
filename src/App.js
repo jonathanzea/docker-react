ab3e9e6b9ba2392d2a5639224c9a80667a3eb770
jonathanzea <jonathanzea91@gmail.com>
2019-11-07 21:14:46 +0100

    Change App test

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          <h1>:D</h1>
+          <h1>I was changed on the feature branch :D</h1>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
